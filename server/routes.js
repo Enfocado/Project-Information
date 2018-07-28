34bfa096dac1db1984cfb67af6040440a38524dd
amanvaid94@gmail.com <amanvaid94@gmail.com>
2018-07-27 22:11:03 -0700

    removed post from route for fill data

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,5 @@ const router = require('express').Router();
 const controller = require('../db/controllers/index.js');
 
 router.get('/fillData', controller.fillData.get);
-router.post('/fillData', controller.fillData.post);
 
 module.exports = router;
