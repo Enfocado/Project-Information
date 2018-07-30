59ae3a94ba262780b1b1162ef7de69c658728f30
amanvaid94@gmail.com <amanvaid94@gmail.com>
2018-07-29 19:07:19 -0700

    Renamed test for default endpoint

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 
 const app = require('../index.js');
 
-describe('Test /', () => {
+describe('Check Default Endpoint /', () => {
   it('should return status code of 200', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
