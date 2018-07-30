bb2529a4cd735c5801ba0c6d34cccdc896f0d881
amanvaid94@gmail.com <amanvaid94@gmail.com>
2018-07-30 11:53:32 -0700

    Add Server test for project enpoint

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,9 +2,14 @@ const request = require('supertest');
 
 const app = require('../index.js');
 
-describe('Check Default Endpoint /', () => {
-  it('should return status code of 200', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
+describe('Check Default Endpoint', () => {
+  it('should return status code of 200', () => {
+    request(app).get('/').expect(200);
+  });
+});
+
+describe('Check project Endpoint', () => {
+  it('should return status code of 200', () => {
+    request(app).get('/project/1').expect(200);
   });
 });
