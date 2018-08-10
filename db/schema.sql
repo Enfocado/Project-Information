d17e2b9dcf528814bd85434c8ff2b407b36d4105
alilthumuch <alilthumuch@gmail.com>
2018-08-10 15:02:37 -0700

    Adding new keys to schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,18 +3,28 @@ CREATE DATABASE projectinformation;
 \c projectinformation;
 
 CREATE TABLE project (
-  projectName text,
-  projectID bigserial,
-  PRIMARY KEY(projectID)
+ projectname text,
+ projectid bigserial,
+ start_date timestamp,
+ end_date timestamp,
+ funding_goal integer,
+ current_goal integer,
+ backers integer,
+ creator_id integer,
+ location text,
+ is_followed boolean,
+ category text,
+ video_link text,
+ PRIMARY KEY(projectID)
 );
 
 CREATE TABLE creator (
-  creatorID bigserial,
-  Company_Name text,
-  Location text,
-  Description text,
-  Person_Name text,
-  Facebook text,
-  Company_Logo text,
-  PRIMARY KEY(creatorID)
+ creatorid bigserial,
+ company_name text,
+ location text,
+ description text,
+ person_name text,
+ facebook text,
+ company_logo text,
+ PRIMARY KEY(creatorid)
 );
